668d722182f13862386ea9bab317efee33bf73a3
Durgathangaraj <124855357+Durgathangaraj@users.noreply.github.com>
2024-04-24 15:17:36 +0530

    Add files via upload

---
+++ Day 4/sec1/src/main/java/com/example/App.java
@@ -0,0 +1,35 @@
+package com.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws InterruptedException
+    {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver1 = new ChromeDriver();
+        driver1.get("https://www.google.co.in/");
+        Thread.sleep(3000);
+        driver1.close();
+        WebDriverManager.firefoxdriver().setup();
+        WebDriver driver2 = new FirefoxDriver();
+        driver2.get("https://www.google.co.in/");
+        Thread.sleep(3000);
+        driver2.close();
+        WebDriverManager.edgedriver().setup();
+        WebDriver driver3 = new EdgeDriver();
+        driver3.get("https://www.google.co.in/");
+        Thread.sleep(3000);
+        driver3.close();
+
+    }
+}
